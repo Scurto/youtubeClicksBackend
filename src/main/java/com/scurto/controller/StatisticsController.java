86c8a86922ea6994def2678e2e3f1272d2aefa91
ustymenkoy <ustymenkoy@digicode.net>
2018-09-18 14:19:56 +0300

    added StatisticsService rest

--- src/main/java/com/scurto/controller/StatisticsController.java
+++ src/main/java/com/scurto/controller/StatisticsController.java
@@ -1,11 +1,14 @@
 package com.scurto.controller;
 
 import com.scurto.model.Website;
+import com.scurto.model.statistics.TopStrategy;
 import com.scurto.service.StatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/statistics")
@@ -17,12 +20,29 @@ public class StatisticsController {
 
     @RequestMapping(value = "/listWebsites", method = RequestMethod.POST)
     @ResponseBody
-    public ArrayList<Website> getWebsites() {
+    public List<Website> getWebsites(@RequestBody TopStrategy topStrategy) {
         try {
-            ArrayList<Website> webSiteList = (ArrayList<Website>) service.getWebsites();
-            return webSiteList;
+            ArrayList<Website> baseList = (ArrayList<Website>) service.getWebsites();
+            System.out.println(topStrategy.getTopStrategy());
+            List<Website> sortedList = new ArrayList<>();
+            switch (topStrategy.getTopStrategy()) {
+                case "top5":
+                    sortedList = baseList.stream()
+                            .filter(elem -> elem.getExecutionCount() != null)
+                            .sorted((f1, f2) ->  Integer.compare(f2.getExecutionCount(), f1.getExecutionCount()))
+                            .limit(2)
+                            .collect(Collectors.toList());
+                    return sortedList;
+                case "top10":
+
+                    break;
+                default:
+                    return sortedList;
+            }
+
         } catch (Exception ex) {
-            return null;
+            System.out.println(ex);
         }
+        return null;
     }
 }
---
+++ src/main/java/com/scurto/model/statistics/TopStrategy.java
@@ -0,0 +1,21 @@
+package com.scurto.model.statistics;
+
+public class TopStrategy {
+
+    private String topStrategy;
+
+    public String getTopStrategy() {
+        return topStrategy;
+    }
+
+    public void setTopStrategy(String topStrategy) {
+        this.topStrategy = topStrategy;
+    }
+
+    @Override
+    public String toString() {
+        return "TopStrategy{" +
+                "topStrategy='" + topStrategy + '\'' +
+                '}';
+    }
+}
